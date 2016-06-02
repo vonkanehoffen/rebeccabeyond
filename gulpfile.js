febd10b8b6000539a51d2831f160d36717e39940
Kane Clover <kane@assure24.com>
2016-06-02 22:39:35 +0100

    child theme + gulp skeleton

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync');
+
+// ... variables
+
+gulp.task('dev', ['sass'], function() {
+
+    browserSync.init({
+        proxy: 'http://rebeccabeyond.loc'
+    });
+
+    gulp.watch('**/*.scss', ['sass']);
+    gulp.watch('**/*.php').on('change', browserSync.reload);
+});
+
+gulp.task('sass', function () {
+  return gulp
+    .src('./style.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./'))
+    .pipe(browserSync.stream({match: '**/*.css'}));
+});

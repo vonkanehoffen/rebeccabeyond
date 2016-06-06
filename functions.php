207d8e1c32ff9cb9a0fcc2ced8ce6fa1eda828eb
Kane Clover <kane@assure24.com>
2016-06-06 12:40:21 +0100

    remove sorting dropdown on shop loop

--- functions.php
+++ functions.php
@@ -5,9 +5,25 @@
 add_action('init', 'rebeccabeyond_custom');
 
 function rebeccabeyond_custom() {
+
+  // Simplify header
   remove_action('storefront_header', 'storefront_product_search', 40);
   remove_action('storefront_header', 'storefront_header_cart', 60);
   remove_action('storefront_header', 'storefront_secondary_navigation', 30);
+
+  // Remove sorting dropdown on shop loop
+  remove_action( 'woocommerce_before_shop_loop',       'storefront_sorting_wrapper',             9 );
+  remove_action( 'woocommerce_before_shop_loop',       'woocommerce_catalog_ordering',           10 );
+  remove_action( 'woocommerce_before_shop_loop',       'woocommerce_result_count',               20 );
+  remove_action( 'woocommerce_before_shop_loop',       'storefront_woocommerce_pagination',      30 );
+  remove_action( 'woocommerce_before_shop_loop',       'storefront_sorting_wrapper_close',       31 );
+
+  remove_action( 'woocommerce_after_shop_loop',        'storefront_sorting_wrapper',             9 );
+  remove_action( 'woocommerce_after_shop_loop',        'woocommerce_catalog_ordering',           10 );
+  remove_action( 'woocommerce_after_shop_loop',        'woocommerce_result_count',               20 );
+  // remove_action( 'woocommerce_after_shop_loop',        'woocommerce_pagination',                 30 );
+  remove_action( 'woocommerce_after_shop_loop',        'storefront_sorting_wrapper_close',       31 );
+
 }
 
 // add_action('wp_enqueue_scripts', 'rebeccabeyond_scripts', 20);
